fa046fb8182161bf499d48a151a28a907ec92dbb
Piyush123-grumpy <piyush.ratna.64@gmail.com>
2021-06-26 13:22:52 +0545

    new change

--- Question 3.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter the value of a :"))
-b=int(input("Enter the value of b :"))
-c=int(input("Enter the value of c :"))
-d=int(input("Enter the value of d :"))
-if a==b or c==d:
-    print("Hello")
